5a8142d62fc0e1d112c846b15e99d9b7dd8def70
nullhashpixel <nullhashpixel@protonmail.com>
2023-11-02 13:00:29 +0100

    resume from last tx found in logs

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-NODE_SOCKET=/cardano/mainnet/ipc/
-mkdir -p ouralogs
-docker run --mount type=bind,source=$NODE_SOCKET,target=/node-ipc --mount type=bind,source=${PWD}/daemon.toml,target=/etc/oura/daemon.toml -v ${PWD}/ouralogs:/logoutput -it ghcr.io/txpipe/oura:latest daemon
+docker stop oura_handlescraper
+docker rm oura_handlescraper
+OURALOGS_PATH=$( cat path.env )
+cat $( ls -1rt ${OURALOGS_PATH}/ouralogs* | tail -n1 ) | tail -n1 > last_tx
+BLOCK_HASH=$( cat last_tx | jq -r .context.block_hash )
+SLOT=$( cat last_tx | jq -r .context.slot )
+echo "starting from ${BLOCK_HASH} ${SLOT}..."
+cat daemon.template | sed "s/{block}/${BLOCK_HASH}/g" | sed "s/{slot}/${SLOT}/g" | tee daemon.toml
+docker run --name oura_handlescraper --mount type=bind,source=${PWD}/daemon.toml,target=/etc/oura/daemon.toml -v ${OURALOGS_PATH}:/logoutput -it ghcr.io/txpipe/oura:latest daemon
